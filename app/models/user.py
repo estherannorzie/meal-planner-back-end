e10435897a82d36c4b808f750576d1bca97fcac6
Esther Annorzie <estherannorzie@gmail.com>
2022-07-27 19:27:52 -0400

    Define user model

---
+++ app/models/user.py
@@ -0,0 +1,9 @@
+from app import db
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    meal_plans = db.relationship("MealPlan", back_populates="user")
