a613713ef44293e3f58a9e20626e010c145277db
Esther Annorzie <estherannorzie@gmail.com>
2022-07-27 19:27:34 -0400

    Define meal plan model

--- app/models/meal_plan.py
+++ app/models/meal_plan.py
@@ -1,1 +1,6 @@
 from app import db
+
+class MealPlan(db.Model):
+    meal_plan_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    user = db.relationship("User", back_populates='meal plans')
