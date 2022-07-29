294972e42b5bb78277f68c4ce4eeff11c2f4ff03
Esther Annorzie <estherannorzie@gmail.com>
2022-07-28 20:18:23 -0400

    Add additional attributes to meal plan model

--- app/models/meal_plan.py
+++ app/models/meal_plan.py
@@ -2,5 +2,9 @@ from app import db
 
 class MealPlan(db.Model):
     meal_plan_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    meal = db.Column(db.String, nullable=False)
+    type = db.Column(db.String, nullable=False)
+    calories = db.Column(db.Integer)
+    diet = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
     user = db.relationship("User", back_populates='meal plans')
