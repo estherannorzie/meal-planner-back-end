bdd3c281bb1687d2c84319b7fa630e768b1fd491
Esther Annorzie <estherannorzie@gmail.com>
2022-07-29 15:10:50 -0400

    Update meal plan type and diet attributes from string to integer

--- app/models/meal_plan.py
+++ app/models/meal_plan.py
@@ -3,8 +3,8 @@ from app import db
 class MealPlan(db.Model):
     meal_plan_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
-    type = db.Column(db.String, nullable=False)
+    type = db.Column(db.Integer, nullable=False)
     calories = db.Column(db.Integer)
-    diet = db.Column(db.String)
+    diet = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey("user.user_id"), nullable=False)
     user = db.relationship("User", back_populates="meal_plans")
