952a3f8c5b1e093de57efc07f86f417cbf8e131c
Esther Annorzie <estherannorzie@gmail.com>
2022-07-29 16:46:25 -0400

    Import function to create user safely and call during creation

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,18 +1,21 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
 from app.models.user import User
+from app.helper_functions import create_user_safely
 
 users_bp = Blueprint("users_bp", __name__, url_prefix="/users")
 
 @users_bp.route("", methods=("POST",))
 def create_user():
     request_body = request.get_json()
-    user = User(
-        username=request_body["username"],
-        first_name=request_body["first_name"],
-        last_name=request_body["last_name"],
-        email=request_body["email"],
-    )
+
+    user = create_user_safely(User, request_body)
+    # user = User(
+    #     username=request_body["username"],
+    #     first_name=request_body["first_name"],
+    #     last_name=request_body["last_name"],
+    #     email=request_body["email"],
+    # )
 
     db.session.add(user)
     db.session.commit()
