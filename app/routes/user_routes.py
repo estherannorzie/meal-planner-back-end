5f28b553ce65193622cf8e05b9befcb17a24569f
Esther Annorzie <estherannorzie@gmail.com>
2022-07-29 16:08:08 -0400

    Fix users blueprint and decorator

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -2,9 +2,9 @@ from flask import Blueprint, request, jsonify, make_response
 from app import db
 from app.models.user import User
 
-user_bp = Blueprint("users", __name__, url_prefix="/users")
+users_bp = Blueprint("users_bp", __name__, url_prefix="/users")
 
-@user_bp.route("", methods=("POST",))
+@users_bp.route("", methods=("POST",))
 def create_user():
     request_body = request.get_json()
     user = User(
