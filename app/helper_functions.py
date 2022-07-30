cab023342311528b545205ce1a3aa9897536cd00
Esther Annorzie <estherannorzie@gmail.com>
2022-07-29 20:06:20 -0400

    Attempt to check if username or email exists in db, create function to display error message

--- app/helper_functions.py
+++ app/helper_functions.py
@@ -1,8 +1,16 @@
+import email
 from flask import jsonify, abort, make_response
+from sqlalchemy import select
 
 def create_user_safely(cls, data_dict):
-    # iterate through username column
+    email_exists = select(cls).where(cls.email==data_dict[email]).exists()
+    username_exists = select(cls).where(cls.username==data_dict[username]).exists()
+
     # if the username is equal to the user's username input abort message
-    # iterate through email column
-    # if the email is equal to the user's email input abort message
-    pass
+    if email_exists or username_exists:
+        create_error_message("Username or email already in use.", 400)
+    else:
+        return data_dict
+
+def create_error_message(message, status_code):
+    abort(make_response(jsonify({"message": message}), status_code))
