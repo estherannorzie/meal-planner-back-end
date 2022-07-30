017394d71785f96d6a1bfabc8898b2484feca18e
Esther Annorzie <estherannorzie@gmail.com>
2022-07-30 02:55:00 -0400

    Create method to get all users

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
 from app.models.user import User
-from app.helper_functions import create_user_safely
+from app.helper_functions import create_user_safely, create_success_message
 
 users_bp = Blueprint("users_bp", __name__, url_prefix="/users")
 
@@ -21,3 +21,10 @@ def create_user():
     db.session.commit()
 
     return make_response(f"User {user.username} successfully created", 201)
+
+
+@users_bp.route("", methods=("GET",))
+def get_all_users():
+    users = User.query.all()
+    response_data = [user.to_dict() for user in users]
+    return make_response(jsonify(response_data))
