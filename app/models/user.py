ed61839559b9dcc4ddbb6e3330c8725c0a4a6d9c
Esther Annorzie <estherannorzie@gmail.com>
2022-07-30 02:11:54 -0400

    Create method transforming user data into a dict

--- app/models/user.py
+++ app/models/user.py
@@ -7,3 +7,12 @@ class User(db.Model):
     last_name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
     meal_plans = db.relationship("MealPlan", back_populates="user")
+
+
+    def to_dict(self):
+        return dict(
+            id=self.user_id,
+            username=self.username,
+            last_name=self.last_name,
+            email=self.email
+        )
