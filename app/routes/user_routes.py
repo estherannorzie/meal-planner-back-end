bf4a8fdc84cbad226706784df3be2df5c00a0ca4
Esther Annorzie <estherannorzie@gmail.com>
2022-07-30 16:55:06 -0400

    Allow get routes to utilize successful message

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
 from app.models.user import User
-from app.helper_functions import create_user_safely, get_record_by_id
+from app.helper_functions import create_user_safely, get_record_by_id, create_success_message
 
 users_bp = Blueprint("users_bp", __name__, url_prefix="/users")
 
@@ -22,17 +22,19 @@ def delete_user(user_id):
     db.session.delete(user)
     db.session.commit()
     
-    return make_response(f"User {user.username} with id of {user_id} successfully deleted", 201)
+    return make_response(f"User {user.username} with id of {user_id} successfully deleted", 200)
 
 
 @users_bp.route("", methods=("GET",))
 def get_all_users():
     users = User.query.all()
     response_data = [user.to_dict() for user in users]
-    return make_response(jsonify(response_data))
+    return create_success_message(response_data, 200)
+    # return make_response(jsonify(response_data))
 
 
 @users_bp.route("/<user_id>", methods=("GET",))
 def get_user(user_id):
     user = get_record_by_id(User, user_id)
-    return make_response(jsonify({"user": user.to_dict()}))
+    return create_success_message(user.to_dict(), 200)
+    # return make_response(jsonify(user.to_dict()), 200)
