6401aec1f5afaae8aa65102c866bae76f76d1c20
Esther Annorzie <estherannorzie@gmail.com>
2022-07-31 16:14:52 -0400

    Rename meal plan blueprint variable and debugging name

--- app/routes/meal_plan_routes.py
+++ app/routes/meal_plan_routes.py
@@ -3,15 +3,15 @@ from app import db
 from app.models.meal_plan import MealPlan
 from app.helper_functions import get_record_by_id, create_meal_plan_safely
 
-meal_plan_bp = Blueprint("meal_plan_bp", __name__, url_prefix="/<user_id>/meal_plans")
+meal_plans_bp = Blueprint("meal_plans", __name__, url_prefix="/meal_plans")
 
-@meal_plan_bp.route("", methods=("POST",))
-def create_meal_plan(user_id):
+@meal_plans_bp.route("/<user_id>/meal_plans", methods=("POST",))
+def add_meal_plan_to_user(user_id):
     user = get_record_by_id(MealPlan, user_id)
+
     request_body = request.get_json()
     meal_plan = create_meal_plan_safely(MealPlan, request_body)
 
-    db.session.add(meal_plan)
-    db.session.commit()
+    db.session.commit(meal_plan)
 
-    return make_response(f"{meal_plan.title} meal plan for user {} successfully created.", 201)
+    return make_response(f"{meal_plan.title} meal plan for user {user.username} successfully created.", 201)
