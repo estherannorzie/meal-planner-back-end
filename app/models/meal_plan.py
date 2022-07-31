b0e4b24c5dc80d4e93ffb9bca6f7c2efb19ef599
Esther Annorzie <estherannorzie@gmail.com>
2022-07-31 15:48:26 -0400

    Construct class method to create meal plan

--- app/models/meal_plan.py
+++ app/models/meal_plan.py
@@ -8,3 +8,19 @@ class MealPlan(db.Model):
     diet = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey("user.user_id"), nullable=False)
     user = db.relationship("User", back_populates="meal_plans")
+
+
+    @classmethod
+    def from_dict(cls, data_dict):
+        try:
+            return cls(
+                title=data_dict["title"],
+                type=data_dict["type"],
+                calories=data_dict["calories"],
+                diet=data_dict["diet"]
+            )
+        except KeyError:
+            return cls(
+                title=data_dict["title"],
+                type=data_dict["type"]
+            )
