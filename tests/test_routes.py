495c7e1b5ee11e710f42ab5d65dae0bfba4ce2f3
Esther Annorzie <estherannorzie@gmail.com>
2022-08-04 19:21:38 -0400

    Create test to read all users

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -8,6 +8,38 @@ def test_get_all_users_with_no_records(client):
     assert response_body == []
 
 
+def test_get_all_users_with_records(client, saved_users):
+    # Act
+    response = client.get("/users")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [
+        {
+            "id": 1,
+            "username": "eggpioneer10",
+            "first_name": "Vance",
+            "last_name": "Lehner",
+            "email": "Vance.Lehner90@gmail.com"
+        },
+        {
+            "id": 2,
+            "username": "fishalienpie",
+            "first_name": "Lora",
+            "last_name": "Kassulke",
+            "email": "Lora.Kassulke75@yahoo.com"
+        },
+        {
+            "id": 3,
+            "username": "nailspotato",
+            "first_name": "Katelin",
+            "last_name": "Gulgowski",
+            "email": "Katelin_Gulgowski94@hotmail.com"
+        }
+    ]
+
+
 def test_get_one_user(client, saved_users):
     # Act
     response = client.get("/users/1")
