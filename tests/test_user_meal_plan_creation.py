4cc8a99edeb197e99e93641350a819ff939b4e78
Esther Annorzie <estherannorzie@gmail.com>
2022-08-05 18:45:19 -0400

    Fix ValueError, to get user meal creation working

--- tests/test_user_meal_plan_creation.py
+++ tests/test_user_meal_plan_creation.py
@@ -4,16 +4,16 @@ import datetime
 def test_create_one_user_meal_plan(client, saved_users, saved_users_meal_plans):
      # Act
     response = client.post("/users/1/meal_plans", json={
-        "title": "Oscar Mayer Extra Cheesy Pizza Lunchables",
+        "title": "Krispy Kreme Doughnuts Original Glazed Doughnut",
         "type": 4,
-        "calories": 280,
-        "date": datetime.date.today()
+        "calories": 190,
+        "date": datetime.date.today().strftime("%Y-%m-%d")
     })
     response_body = response.get_json()
 
     # Assert
     assert response.status_code == 201
-    assert response_body == "Meal_plan Oscar Mayer Extra Cheesy Pizza Lunchables for user eggpioneer10 successfully created."
+    assert response_body == "Krispy Kreme Doughnuts Original Glazed Doughnut meal plan for user eggpioneer10 successfully created."
 
 
 @pytest.mark.skip(reason="No way to test this feature yet")
