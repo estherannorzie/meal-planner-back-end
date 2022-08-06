7f53da28af2d539319167b67fd3be0696de64c0a
Esther Annorzie <estherannorzie@gmail.com>
2022-08-06 14:26:10 -0400

    Add test to make sure user ID is valid before deletion

--- tests/test_user_meal_plan_deletion.py
+++ tests/test_user_meal_plan_deletion.py
@@ -20,3 +20,19 @@ def test_abort_deleting_invalid_user_meal_plan(client, saved_users_meal_plans):
 
     assert response.status_code == 400
     assert response_body == "Meal plan ID: 1o is not a valid ID."
+
+
+def test_abort_deleting_meal_plan_if_user_nonexistent(client, saved_users_meal_plans):
+    response = client.delete("/users/4/meal_plans/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == "User ID: 4 does not exist."
+
+
+def test_abort_deleting_meal_plan_if_user_nonexistent(client, saved_users_meal_plans):
+    response = client.delete("/users/1000/meal_plans/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == "User ID: 1000 does not exist."
