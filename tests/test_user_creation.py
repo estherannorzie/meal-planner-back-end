68dbb87a8ee6fe5cd89188df1a2dff3972dd7127
Esther Annorzie <estherannorzie@gmail.com>
2022-08-06 15:37:11 -0400

    Create tests to verify constraints work for user creation

--- tests/test_user_creation.py
+++ tests/test_user_creation.py
@@ -40,7 +40,7 @@ def test_user_creation_aborts_if_unneeded_properties_present(client):
     assert response.status_code == 400
     assert response_body == "Additional unneeded attributes submitted. Try again."
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+
 def test_user_creation_aborts_if_username_in_use(client, saved_users):
     response = client.post("/users", json={
         "username": "nailspotato",
@@ -51,8 +51,69 @@ def test_user_creation_aborts_if_username_in_use(client, saved_users):
     response_body = response.get_json()
 
     assert response.status_code == 400
-    assert response_body == None
+    assert response_body == "Already in use. Try again."
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+
 def test_user_creation_aborts_if_email_in_use(client, saved_users):
-    pass
+    response = client.post("/users", json={
+        "username": "DoneDynasty097",
+        "first_name": "Erin",
+        "last_name": "Patton",
+        "email": "Katelin_Gulgowski94@hotmail.com",
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == "Already in use. Try again."
+
+
+def test_user_creation_aborts_if_username_too_long(client):
+    response = client.post("/users", json={
+        "username": "a_veryveryveryveryveryveryveryveryveryveryvveryveryveryveryveryveryveryveryveryverylong_username",
+        "first_name": "Darron",
+        "last_name": "Holt",
+        "email": "Darron_Holt40@gmail.com",
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == "An attribute is too long. Try again."
+
+
+def test_user_creation_aborts_if_email_too_long(client):
+    response = client.post("/users", json={
+        "username": "HolyTyphoon439",
+        "first_name": "Darron",
+        "last_name": "Holt",
+        "email": "a_veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryverylong_email@gmail.com",
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == "An attribute is too long. Try again."
+
+
+def test_user_creation_aborts_if_first_name_too_long(client):
+    response = client.post("/users", json={
+        "username": "HolyTyphoon439",
+        "first_name": "a_long_first_name",
+        "last_name": "Holt",
+        "email": "Darron_Holt40@gmail.com",
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == "An attribute is too long. Try again."
+
+
+def test_user_creation_aborts_if_last_name_too_long(client):
+    response = client.post("/users", json={
+        "username": "HolyTyphoon439",
+        "first_name": "Darron",
+        "last_name": "a__long_last_name",
+        "email": "Darron_Holt40@gmail.com",
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == "An attribute is too long. Try again."
