a39a2519d173f821656a05e2437f4d0c57fb8938
Esther Annorzie <estherannorzie@gmail.com>
2022-08-06 15:59:16 -0400

    Create test to check that required attributes are present for user meal plan creation

--- tests/test_user_meal_plan_creation.py
+++ tests/test_user_meal_plan_creation.py
@@ -9,7 +9,6 @@ def test_create_one_user_meal_plan(client, saved_users):
     })
     response_body = response.get_json()
 
-    # Assert
     assert response.status_code == 201
     assert response_body == "Krispy Kreme Doughnuts Original Glazed Doughnut meal plan for user eggpioneer10 successfully created."
 
@@ -40,7 +39,7 @@ def test_user_cannot_create_past_meal_plans(client, saved_users):
     assert response_body == "Meal plans cannot be created in the past."
 
 
-def test_invalid_user_cannot_create_meal_plan(client):
+def test_invalid_user_cannot_create_meal_plan(client, saved_users):
     response = client.post("/users/1oo/meal_plans", json={
         "title": "Sandwich, Ham (4 oz), Cheese (4 oz), mayo, 3 oz Bread",
         "type": 2,
@@ -64,3 +63,40 @@ def test_nonexistent_user_cannot_create_meal_plan(client, saved_users):
 
     assert response.status_code == 404
     assert response_body == "User ID: 5 does not exist."
+
+
+def test_abort_user_meal_plan_creation_title_not_present(client, saved_users):
+    response = client.post("/users/2/meal_plans", json={
+        "type": 2,
+        "calories": 800,
+        "date": date.today().strftime("%Y-%m-%d")
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == "Missing title."
+
+
+def test_abort_user_meal_plan_creation_type_not_present(client, saved_users):
+    response = client.post("/users/2/meal_plans", json={
+        "title": "Sandwich, Ham (4 oz), Cheese (4 oz), mayo, 3 oz Bread",
+        "calories": 800,
+        "date": date.today().strftime("%Y-%m-%d")
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == "Missing type."
+
+
+def test_abort_user_meal_plan_creation_title_too_long(client, saved_users):
+    response = client.post("/users/2/meal_plans", json={
+        "title": "Stuffed Zucchini is Extremely Delicious, it will help me get my nutrition",
+        "type": 3,
+        "calories": 570,
+        "date": date.today().strftime("%Y-%m-%d")
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == "An attribute is too long. Try again."
