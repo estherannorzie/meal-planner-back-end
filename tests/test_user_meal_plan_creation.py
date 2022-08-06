531df393ad9523de1d3e4891073458e24666ccd3
Esther Annorzie <estherannorzie@gmail.com>
2022-08-06 01:58:17 -0400

    Create tests so user meal plans cannot be created in the past and check for valid user ID

--- tests/test_user_meal_plan_creation.py
+++ tests/test_user_meal_plan_creation.py
@@ -1,13 +1,11 @@
-import pytest
-import datetime
+from datetime import date
 
-def test_create_one_user_meal_plan(client, saved_users, saved_users_meal_plans):
-     # Act
+def test_create_one_user_meal_plan(client, saved_users):
     response = client.post("/users/1/meal_plans", json={
         "title": "Krispy Kreme Doughnuts Original Glazed Doughnut",
         "type": 4,
         "calories": 190,
-        "date": datetime.date.today().strftime("%Y-%m-%d")
+        "date": date.today().strftime("%Y-%m-%d")
     })
     response_body = response.get_json()
 
@@ -21,22 +19,48 @@ def test_creating_user_meal_plan_aborts_if_meal_plan_already_exists(client, save
         "title": "Spaghetti & Meatballs with Tomato Sauce, small",
         "type": 3,
         "calories": 412,
-        "date": datetime.date.today().strftime("%Y-%m-%d")
+        "date": date.today().strftime("%Y-%m-%d")
     })
     response_body = response.get_json()
 
-    # Assert
     assert response.status_code == 400
     assert response_body == "Duplicate meal plans are not allowed."
 
-@pytest.mark.skip(reason="Do later")
+
 def test_user_cannot_create_past_meal_plans(client, saved_users):
-    pass
+    response = client.post("/users/2/meal_plans", json={
+        "title": "Frosted Flakes Ready-to-Eat Cereal",
+        "type": 1,
+        "calories": 143,
+        "date": date(2000, 1, 1).strftime("%Y-%m-%d")
+    })
+    response_body = response.get_json()
 
-@pytest.mark.skip(reason="Do later")
-def test_invalid_user_cannot_create_meal_plan(client, saved_users):
-    pass
+    assert response.status_code == 400
+    assert response_body == "Meal plans cannot be created in the past."
 
-@pytest.mark.skip(reason="Do later")
+
+def test_invalid_user_cannot_create_meal_plan(client):
+    response = client.post("/users/1oo/meal_plans", json={
+        "title": "Sandwich, Ham (4 oz), Cheese (4 oz), mayo, 3 oz Bread",
+        "type": 2,
+        "calories": 800,
+        "date": date.today().strftime("%Y-%m-%d")
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == "User ID: 1oo is not a valid ID."
+
+
 def test_nonexistent_user_cannot_create_meal_plan(client, saved_users):
-    pass
+    response = client.post("/users/5/meal_plans", json={
+        "title": "Stuffed Zucchini",
+        "type": 3,
+        "calories": 570,
+        "date": date.today().strftime("%Y-%m-%d")
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == "User ID: 5 does not exist."
