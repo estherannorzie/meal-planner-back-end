b69b32c2a64a15dd34efd85e3feda7fa20860047
Esther Annorzie <estherannorzie@gmail.com>
2022-08-07 10:31:32 -0400

    Delete unneeded route

--- app/routes/meal_plan_routes.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint, request, jsonify, make_response
-from app import db
-from app.models.meal_plan import MealPlan
-
-meal_plans_bp = Blueprint("meal_plans", __name__, url_prefix="/meal_plans")
