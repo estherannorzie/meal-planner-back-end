acd82126736fa05bcebd47d27c18696a03910013
Esther Annorzie <estherannorzie@gmail.com>
2022-08-09 17:24:07 -0400

    Update user model to include password with a min length constraint

--- app/models/user.py
+++ app/models/user.py
@@ -1,12 +1,27 @@
 from app import db
+from sqlalchemy.schema import CheckConstraint
+from sqlalchemy.orm import validates
 
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(16), nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
     first_name = db.Column(db.String(16), nullable=False)
     last_name = db.Column(db.String(16), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     meal_plans = db.relationship("MealPlan", back_populates="user")
+
+    # https://stackoverflow.com/questions/50174325/define-minimum-length-for-postgresql-string-column-with-sqlalchemy
+    __table_args__ = (
+        CheckConstraint("char_length(password) > 11",
+                        name="password_min_length"),
+    )
+
+    @validates(password)
+    def validate_password(self, key, password) -> str:
+        if len(password) < 12:
+            raise ValueError("Password is too short.")
+        return password
 
 
     @staticmethod
@@ -17,6 +32,7 @@ class User(db.Model):
         return dict(
             id=self.user_id,
             username=self.username,
+            password=self.password,
             first_name=self.first_name,
             last_name=self.last_name,
             email=self.email
@@ -26,6 +42,7 @@ class User(db.Model):
     def from_dict(cls, data_dict):
         return cls(
             username=data_dict["username"],
+            password=data_dict["password"],
             first_name=data_dict["first_name"],
             last_name=data_dict["last_name"],
             email=data_dict["email"]
